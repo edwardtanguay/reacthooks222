4b1ca34841388f3715fcbf304d7b28521b0079d0
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-11 00:13:36 +0100

    use state

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,10 +1,12 @@
-// import React from 'react';
+import {useState} from 'react';
 
 function InfoBox(props) {
+	const [ noteState, setNoteState ] = useState('hide');
 	return (
 		<div>
 			<h1>{props.title}</h1>
 			<p>Info Box</p>
+			<p>Note State: {noteState}</p>
 		</div>	
 	);
 }

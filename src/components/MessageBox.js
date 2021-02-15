50dffc3e28aadc50c5001ca5c38e5c6144cc338c
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-15 23:11:34 +0100

    make checkbox work

--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import '../styles/MessageBox.scss';
 
-function MessageBox({framed}) {
+function MessageBox({ framed }) {
 	const [background, setBackground] = useState(false);
+	const [checked, setChecked] = useState(false);
 	return (
 		<div className={`component_messageBox ${framed === 'true' ? 'component_messageBox_framed' : 'component_messageBox_normal'}
 		${background ? 'component_messageBox_background' : 'component_messageBox_normal'}
@@ -11,7 +12,10 @@ function MessageBox({framed}) {
 			<p>This is a message box.</p>
 			<p>Frame: {framed} </p>
 			<p>Background: {background ? 'on' : 'off'} </p>
-			<button onClick={() => setBackground(background ? false : true)}>Background</button>
+			<p>
+				<button onClick={() => setBackground(background ? false : true)}>Background</button>
+			</p>
+			<p><input type="checkbox" checked={checked ? 'checked' : ''} onChange={() => setChecked(!checked)}/>Checked: {checked ? 'checked' : 'unchecked'}</p>
 		</div>
 	)
 }

2047703e4154c709b036c3208d2088ec01bcbd2f
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-15 23:49:39 +0100

    add Star

---
+++ src/components/Star.js
@@ -0,0 +1,9 @@
+import { FaStar } from 'react-icons/fa';
+
+function Star() {
+	return (
+		<FaStar color="yellow" />
+	)
+}
+
+export default Star;
--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,11 +1,12 @@
-import { FaStar } from 'react-icons/fa';
+import Star from './Star';
 
 const createArray = (length) => [...Array(length)];
 
 function Stars({ total }) {
 	return createArray(total).map((n, index) => (
-			<FaStar color="green" key={index} />
+		<Star />
 	));
+
 }
 
 export default Stars;

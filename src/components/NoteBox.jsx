50e47d9b1813996cecdbb71c133e9a22153e1c59
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-16 10:57:22 +0100

    start DataBox

--- src/components/NoteBox.jsx
+++ src/components/NoteBox.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
+
 function NoteBox() {
+	const [name, setName] = useState('Andrea');
+	const [admin] = useState(false);
+	useEffect(() => {
+		document.title = `Congratulations, ${name}!`;
+		console.log(`name: ${name}`)
+	}, [name]);
+	useEffect(() => {
+		console.log(`admin: ${admin ? 'we are admin' : 'we are not admin'}`);
+	}, [admin]);
 	return (
 		<div>
-			Contratulations, name.
+			<p>Contratulations, {name}.</p>
+			<p><button onClick={() => setName('Alex')}>Change winner</button></p>	
+			<p>(Note with UseEffect, the document title also changes.)</p>
 		</div>
 	)
 }

12702834344d9ef7f9af174d599f92d448c0489c
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-16 20:18:55 +0100

    increase increment

--- src/components/ReducerBox.jsx
+++ src/components/ReducerBox.jsx
@@ -1,12 +1,14 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 function ReducerBox() {
+	const [increment, setIncrement] = useState(2);
 	const [count, setCount] = useReducer((count, newCount) => count + newCount, 0);
 	return (
 		<div>
 			<h1>Reducer Box</h1>
 			<p>Current count: {count}</p>
-			<button onClick={() => setCount(1)}>Increment</button>
+			<button onClick={() => setCount(increment)}>Increment</button> 
+			<button onClick={() => setIncrement(increment + 1)}>Increase increment</button>
 		</div>
 	);
 }

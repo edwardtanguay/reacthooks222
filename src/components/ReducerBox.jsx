43ae5a9f2adfb2cccb6eaa43a55c04df24b663b4
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-16 20:28:11 +0100

    has increment in box

--- src/components/ReducerBox.jsx
+++ src/components/ReducerBox.jsx
@@ -1,14 +1,20 @@
 import { useReducer, useState } from 'react';
+import '../styles/ReducerBox.scss';
 
 function ReducerBox() {
 	const [increment, setIncrement] = useState(2);
 	const [count, setCount] = useReducer((count, newCount) => count + newCount, 0);
 	return (
-		<div>
+		<div className="component_reducerBox" >
 			<h1>Reducer Box</h1>
 			<p>Current count: {count}</p>
-			<button onClick={() => setCount(increment)}>Increment</button> 
-			<button onClick={() => setIncrement(increment + 1)}>Increase increment</button>
+			<div>
+				<button onClick={() => setCount(increment)}>Increment</button>
+			</div>
+			<div>
+				<input type="text" value={increment} />
+				<button onClick={() => setIncrement(increment + 1)}>Set increment</button>
+			</div>
 		</div>
 	);
 }

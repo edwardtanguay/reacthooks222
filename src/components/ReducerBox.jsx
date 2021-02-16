567f231301a457d680f93e91ae3745f8c490ee12
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-16 20:13:31 +0100

    userReducer simple

--- src/components/ReducerBox.jsx
+++ src/components/ReducerBox.jsx
@@ -1,10 +1,12 @@
 import { useReducer } from 'react';
 
 function ReducerBox() {
+	const [count, setCount] = useReducer((count, newCount) => count + newCount, 0);
 	return (
 		<div>
 			<h1>Reducer Box</h1>
-			<p>test</p>
+			<p>Current count: {count}</p>
+			<button onClick={() => setCount(1)}>Increment</button>
 		</div>
 	);
 }

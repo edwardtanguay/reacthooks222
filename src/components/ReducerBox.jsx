7bc468c64c826bcc56df74befb83362843769f79
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-16 21:30:20 +0100

    dynamic increment for ReducerBox

--- src/components/ReducerBox.jsx
+++ src/components/ReducerBox.jsx
@@ -2,15 +2,14 @@ import { useReducer, useState } from 'react';
 import '../styles/ReducerBox.scss';
 
 function ReducerBox() {
-	const [increment, setIncrement] = useState(2);
+	const [increment, setIncrement] = useState(1);
 	const [count, setCount] = useReducer((count, newCount) => count + newCount, 0);
 	return (
 		<div className="component_reducerBox" >
 			<h1>Reducer Box</h1>
 			<p>Current count: {count} <button onClick={() => setCount(increment)}>Increment</button></p>
 			<div>
-				<input type="text" defaultValue={increment}/>
-				<button onClick={() => setIncrement(increment + 1)}>Set increment</button>
+				Increment: <input type="text" value={increment} onChange={e => setIncrement(parseInt(e.target.value))}/>
 			</div>
 				
 		</div>

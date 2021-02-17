1b99623b3c558ab4461938a31969e008ca4e1c77
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-02-17 11:49:22 +0100

    UseRefBox works

--- src/components/UseRefBox.jsx
+++ src/components/UseRefBox.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import '../styles/UseRefBox.scss';
 
 function UseRefBox() {
@@ -6,35 +6,29 @@ function UseRefBox() {
 	const name = useRef();
 	const color = useRef();
 
-	const submit = (e) => {
-		e.preventDefault();
-		console.log(name.current.value, color.current.value);
-		elements.push(
+	const submit = () => {
+		setElements([...elements,
 			{
 				name: name.current.value,
 				color: color.current.value
 			}
-		);
-		setElements(elements);
-		console.log(elements);
+		])
 	}
-	useEffect(() => {
-		console.log('here');
-	}, []);
 	return (
 		<div className="component_useRefBox">
 			<h1>UseRef Box</h1>
-			<form onSubmit={submit}>
-				<h2>Page Elements:</h2>
+			<form>
+				<h2>Define Page Elements:</h2>
 				<div><label>Name:</label><input ref={name} type="text" /></div>
 				<div>
 					<label>Color:</label><input ref={color} type="color" />
 				</div>
-				<button>Save</button>
+				<button type="button" onClick={() => submit()}>Save</button>
 			</form>
+			<h2>Currently {elements.length} Elements:</h2>
 			<ul>
 				{elements.map((element) => (
-					<li key={element.name}>nnn</li>
+					<li key={element.name}>{element.name} = {element.color}</li>
 				))}
 			</ul>
 		</div>
